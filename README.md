897d5f514f2abe42080c630bca55057026f8e6a8
prince singh <princesinghgkp984@gmail.com>
2026-03-26 19:12:21 +0530

    Clean project without secrets

---
+++ README.md
@@ -0,0 +1,2 @@
+# Barber-Reservation-System
+Full-stack Barber Reservation System built using Java, Spring Boot, React.js, and MySQL with real-time booking and JWT authentication.
---
+++ frontend/src/util/useNotificationWebsoket.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+import SockJS from 'sockjs-client';
+import { addNotification } from '../Redux/Notifications/action';
+import { useDispatch, useSelector } from 'react-redux';
+import Stomp from "stompjs";
+
+const useNotificationWebsoket = (userId,type) => {
+    
+ 
+    const dispatch=useDispatch()
+
+    const [stompClient, setStompClient] = useState(null);
+    useEffect(() => {
+        if (!userId) return; // Only connect if userId is available
+    
+        const sock = new SockJS("http://localhost:5000/api/notifications/ws");
+        const stomp = Stomp.over(sock);
+        setStompClient(stomp);
+      }, [userId]);
+    
+      useEffect(() => {
+        if (stompClient) {
+          stompClient.connect(
+            {},
+            () => {
+              
+              stompClient.subscribe(
+                `/notification/${type}/${userId}`,
+                onMessageRecive,
+                (error) => {
+                  console.error("Subscription error:", error);
+                }
+              );
+              console.log("Subscribed to notifications for user ID:", userId);
+            },
+            (error) => {
+              console.error("WebSocket error:", error);
+            }
+          );
+        }
+    
+        return () => {
+          if (stompClient?.connected) {
+            stompClient.disconnect(() => {
+              console.log("Disconnected from WebSocket");
+            });
+          }
+        };
+      }, [stompClient, userId]);
+    
+      const onMessageRecive = (payload) => {
+        console.log("New message received", payload);
+        const receivedMessage = JSON.parse(payload.body);
+    
+        // Dispatch the new notification to Redux store
+        dispatch(addNotification(receivedMessage));
+      };
+}
+
+export default useNotificationWebsoket
